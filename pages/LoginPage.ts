44260bc9e006d666b9ce62437325cb3e91fb0541
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-23 17:46:13 +0530

    Added Page Object Model

---
+++ pages/LoginPage.ts
@@ -0,0 +1,15 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  username = this.page.locator('#user-name');
+  password = this.page.locator('#password');
+  loginBtn = this.page.locator('#login-button');
+
+  async login(user: string, pass: string) {
+    await this.username.fill(user);
+    await this.password.fill(pass);
+    await this.loginBtn.click();
+  }
+}
